8f7ae4391f3b27af10e28b7ee671d1ad848a8287
namoosir <saqeebnazmus@gmail.com>
2023-09-07 00:53:43 -0400

    initial api gateway setup

--- Program.cs
+++ Program.cs
@@ -1,8 +1,17 @@
+using StackExchange.Redis;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddSingleton<IConnectionMultiplexer>(
+    opt =>
+        ConnectionMultiplexer.Connect(
+            builder.Configuration.GetConnectionString("RedisConnectionString")
+        )
+);
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
